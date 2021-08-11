1f65de4ceaa4ebc21169cdb41c5dcd132da807aa
citrus789 <86214076+citrus789@users.noreply.github.com>
2021-08-10 22:28:51 -0700

    added initial view profile layout

--- viewprofile.php
+++ viewprofile.php
@@ -16,21 +16,20 @@
     while($row = $result -> fetch_array(MYSQLI_ASSOC)) {
       if($row["Email"] == $username) {
         $firstname = $row['FirstName'];
+        $bio = $row['Bio'];
         $lastname = $row['LastName'];
-        $email = $row['Email'];
+        $email = $row['Email2'];
         $phone = $row['Phone'];
         $country = $row['Country'];
         $region = $row['Region'];
         $city = $row['City'];
-        $level = $row['Level'];
-        $school = $row['School'];
-        $program = $row['Program'];
-        $year = $row['Year'];
-        $gpa = $row['GPA'];
-
-        $position1 = $row['Position1'];
-
         $image = $row['Image'];
+        if(!empty($image)) {
+          $profilepic = "img/".$image;
+        }
+        else {
+          $profilepic = "img/defaultprofile.PNG";
+        }
       }
     }
 
@@ -64,9 +63,74 @@
     </header>
     <div class = "bottomnav">
       <div class = "bottomnavcontents">
-        <div id = "editprofile"><a class = "active" href="editprofile.php">Edit Profile</a></div>
-        <div id = "viewprofile" class = "active"><a href="viewprofile.php">View Profile</a></div>
+        <a class = "active" href="editprofile.php"><div id = "editprofile">Edit Profile</div></a>
+        <a href="viewprofile.php"><div id = "viewprofile" class = "active">View Profile</div></a>
       </div class = "bottomnavcontents">
+    </div>
+    <div class = "viewprofilecontainer">
+      <div class = "viewprofilecontents">
+        <div class = "viewbasicinfo">
+          <div class = "viewimage">
+            <img src="<?php echo $profilepic; ?>" height="200" width="200" border-radius="50%" background-color="white" class="imgthumbnail" />
+          </div>
+          <div class = "viewbasicinfocontents">
+            <div class = "viewname">
+              <?php echo $firstname." ".$lastname; ?>
+            </div>
+            <hr style = "margin-left: 10px; width: 100%; margin-bottom: 10px;" color = "darkblue" size = "3">
+            <div class = "viewcontact">
+              <?php if (!is_null($email)) { echo "Email: ".$email; }
+                    if (!is_null($phone) and !empty($phone)) { echo " | Phone: ".$phone; }?>
+            </div>
+            <div class = "viewlocation">
+              <?php if (!is_null($city) and !empty($city)) { echo $city." "; } ?>
+              <?php if (!is_null($region) and !empty($region)) { echo $region." "; } ?>
+              <?php if (!is_null($country) and !empty($country)) { echo $country; } ?>
+            </div>
+            <div class = "viewbio"><p style = "padding: 5px;">
+              <?php if (!is_null($bio) and !empty($bio)) { echo $bio; } ?>
+            </p></div>
+          </div>
+        </div>
+
+        <div class = "viewskills">
+          <?php
+          $skll = $conn->query("SELECT Email, Skill1, Skill2, Skill3, Skill4, Skill5, Skill6, Skill7 FROM user_login");
+          while($row = $skll -> fetch_array(MYSQLI_NUM)) {
+            if($row[0] == $email) {
+              for ($i = 1; $i < 7; $i++) {
+                if (is_null($row[$i]) or empty($row[$i]) or $row[$i] == "NULL") {
+                  continue;
+                }
+                else {
+                  echo "<div class = skilltag><div class = skillname>".unserialize($row[$i])->skill."</div><div class = skillyear>".unserialize($row[$i])->year."</div></div>";
+                }
+              }
+            }
+          }
+          ?>
+        </div>
+        <div class = "viewexperience">
+          <?php
+          $exp = $conn->query("SELECT Email, Experience1, Experience2, Experience3, Experience4, Experience5 FROM user_login");
+          while($row = $exp -> fetch_array(MYSQLI_NUM)) {
+            if($row[0] == $email) {
+              for ($i = 1; $i < 5; $i++) {
+                if (is_null($row[$i]) or empty($row[$i]) or $row[$i] == "NULL") {
+
+                  continue;
+                }
+                else {
+                  echo "<div class = experiencetag><div class = experiencerole>".unserialize($row[$i])->role."</div><div class = experiencecompany>".unserialize($row[$i])->company."</div><div class = experiencestartend>".unserialize($row[$i])->start." - ".unserialize($row[$i])->end."</div></div>";
+                }
+              }
+            }
+          }
+          ?>
+        </div>
+        <div class = "vieweducation">
+        </div>
+      </div>
     </div>
   </body>
 
