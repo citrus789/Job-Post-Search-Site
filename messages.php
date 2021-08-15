e3e46852de40b477e8d9dfb278f354975e5ce83f
citrus789 <86214076+citrus789@users.noreply.github.com>
2021-08-14 23:39:07 -0700

    multiple "page" posting navigation, added more posting info to card

--- messages.php
+++ messages.php
@@ -34,7 +34,7 @@ if(isset($_SESSION["Email"]) || $_SESSION['loggedin'] == true) {
 
     <title>First Website</title>
     <link rel = "stylesheet" href = "styles.css">
-
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <link rel = "stylesheet" href = "styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -58,26 +58,110 @@ if(isset($_SESSION["Email"]) || $_SESSION['loggedin'] == true) {
       <div class = "postingslist">
         <h1 class = "postingstitle" style = "margin-left: 10px;">Your Postings</h1>
         <?php
-        $i = 0;
+        $numcard = 0;
         //echo $postingcount;
-        for ($i = 0; $i < $postingcount; $i++) {?>
+        for ($i = 0; $i < $postingcount; $i++) {
+
+          if ($numcard == 0 or $numcard % 3 == 0) {
+          ?><div id = "messagecardpage" class = "messagecardpage">
+    <?php } ?>
+
           <div class = "postingcard">
             <div>
               <div class = "maintitle">
                 <div class = "postingtitle"><?php echo unserialize($postinglist[$i])->position." at ".unserialize($postinglist[$i])->company;?></div>
               </div>
-              <div>
+              <div class = postingcardinfo>
                 <div class = "moreinfo">
-                  <div class = "salary"><?php echo "$".unserialize($postinglist[$i])->salarystart." - $".unserialize($postinglist[$i])->salaryend." (".unserialize($postinglist[$i])->currency.")"?></div>
+                  <div class = "salary">
+                    <?php
+                    if (!empty(unserialize($postinglist[$i])->salarystart)) {
+                      echo "$".unserialize($postinglist[$i])->salarystart;
+                      if (!empty(unserialize($postinglist[$i])->salaryend)) {
+                        echo " - $".unserialize($postinglist[$i])->salaryend;
+                      }
+                    }
+                    else {
+                      echo "$".unserialize($postinglist[$i])->wage." / hour";
+                    }
+                    echo " (".unserialize($postinglist[$i])->currency.")";
+                    ?>
+                  </div>
                 </div>
                 <div class = "isremote">
                   <div class = "remote?"><?php echo "".unserialize($postinglist[$i])->remote.""?></div>
                 </div>
+                <?php
+                if (!isset(unserialize($postinglist[$i])->type)) {
+                  $type = '';
+                }
+                else {
+                  $type = unserialize($postinglist[$i])->type;
+                }
+
+                ?>
+                <div class = "isremote">
+                  <div class = "remote?"><?php echo "".$type.""?></div>
+                </div>
+                <div class = "isremote">
+                  <div class = "remote?"><?php if (unserialize($postinglist[$i])->remote == 'In Person') { echo "".unserialize($postinglist[$i])->city.", ".unserialize($postinglist[$i])->region.", ".unserialize($postinglist[$i])->country; }?></div>
+                </div>
               </div>
             </div>
           </div>
-  <?php } ?>
+          <?php $numcard ++;
+          if ($numcard % 3 == 0) {
+            $end = true;
+            ?> </div> <?php
+          }
+          else {
+            $end = false;
+          }
+        }
+        if ($end == false) {
+          ?> </div> <?php
+        }
+        if ($numcard > 3) { ?>
+          <table class = "searchnav" id = "searchnav" style = "height: 25px;">
+            <tr></tr>
+          </table>
       </div>
+      <script>
+        <?php
+        //Warning: unreadable shit below!!!
+
+        for ($i = 1; $i <= ceil($numcard / 3); $i++) { ?>
+          console.log("loop");
+          $("#searchnav").find('tr').each(function() {
+            $(this).append("<td id = searchnav<?php echo $i; ?> name = <?php echo $i; ?> onclick = showpage(this)><?php echo $i; ?></td>");
+          });
+          <?php if ($i == 1) {?>
+            document.getElementById("searchnav1").setAttribute("class", "active");
+            var currentpage = document.getElementsByClassName("messagecardpage");
+            currentpage[0].style.display = "block";
+            <?php for ($j = 1; $j < ceil($numcard / 3); $j++) { ?>
+              currentpage[<?php echo $j;?>].style.display = "none";
+      <?php }
+          }
+        } ?>
+        function showpage(elem) {
+          var table = document.getElementById("searchnav");
+          var currentpage = document.getElementsByClassName("messagecardpage");
+
+          for (let row of searchnav.rows) {
+            for(let cell of row.cells) {
+              cell.removeAttribute("class");
+            }
+          }
+
+          <?php for ($j = 0; $j < ceil($numcard / 3); $j++) { ?>
+            currentpage[<?php echo $j; ?>].style.display = "none";
+          <?php } ?>
+          elem.setAttribute("class", "active");
+          currentpage[Number(elem.getAttribute("name") - 1)].style.display = "block";
+        }
+      </script>
+    <?php } ?>
     </div>
   </body>
 </html>
